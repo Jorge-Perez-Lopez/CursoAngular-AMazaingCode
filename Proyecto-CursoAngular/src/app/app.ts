cd59022c81244624800ad7b27355bc88e4d53ec4
Jorge Pérez <perezlopezj07@hotmail.com>
2026-06-29 12:14:37 +0200

    Add For

--- Proyecto-CursoAngular/src/app/app.ts
+++ Proyecto-CursoAngular/src/app/app.ts
@@ -15,6 +15,24 @@ export class AppComponent {
   number: number = 0;
   conditional: boolean = true;
   conditional2: string = 'Hola';
+  books:  string[] = ['Hades, el dios menos malo', 'Hércules, el héroe que no quiso serlo', 'Sísifo, el hombre que engañó a la muerte'];
+  animals: any = [
+    {
+        id: 1,
+        name: "dog",
+        img: "https://nypost.com/wp-content/uploads/sites/2/2023/03/dog.jpg?quality=75&strip=all&w=1024"
+    },
+    {
+        id: 2,
+        name: "cat",
+        img: "https://img.freepik.com/foto-gratis/lindo-gatito-domestico_155003-16730.jpg?w=2000"
+    },
+    {
+        id: 3,
+        name: "bird",
+        img: "https://media.cnn.com/api/v1/images/stellar/prod/230309163059-01-bird.jpg?q=w_800,h_450,c_fill"
+    }
+]
 
   addOne() {
     this.number++;
---
+++ Proyecto-CursoAngular/src/data/animals.js
@@ -0,0 +1,17 @@
+animals: any = [
+    {
+        id: 1,
+        name: "dog",
+        img: "https://nypost.com/wp-content/uploads/sites/2/2023/03/dog.jpg?quality=75&strip=all&w=1024"
+    },
+    {
+        id: 2,
+        name: "cat",
+        img: "https://img.freepik.com/foto-gratis/lindo-gatito-domestico_155003-16730.jpg?w=2000"
+    },
+    {
+        id: 3,
+        name: "bird",
+        img: "https://media.cnn.com/api/v1/images/stellar/prod/230309163059-01-bird.jpg?q=w_800,h_450,c_fill"
+    }
+]
