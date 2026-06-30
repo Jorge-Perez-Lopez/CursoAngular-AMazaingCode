32f4a2dba1766f4fd6ab8cc95406cca86b54adf9
Jorge Pérez <perezlopezj07@hotmail.com>
2026-06-30 11:58:59 +0200

    Merge Squash: Add Input (#7)

---
+++ Proyecto-CursoAngular/src/app/components/child/child.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.html',
+  styleUrl: './child.css',
+})
+export class ChildComponent {
+    @Input() msg: String = '';
+
+    @Input()
+    person: any
+}
