be820f395ed5325196d954e32b2a2aa55ad1161d
Jorge Pérez <perezlopezj07@hotmail.com>
2026-07-04 18:04:10 +0200

    Add Routes

---
+++ Proyecto-CursoAngular/src/app/pages/movie-form-page/movie-form-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-form-page',
+  imports: [],
+  templateUrl: './movie-form-page.html',
+  styleUrl: './movie-form-page.css',
+})
+export class MovieFormPage {}
