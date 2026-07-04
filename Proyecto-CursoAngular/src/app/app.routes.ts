8ddb09866b80c049a72ee11d17d97e8ea278ce75
Jorge Pérez <perezlopezj07@hotmail.com>
2026-07-04 23:11:25 +0200

    Add Parametric Routes

--- Proyecto-CursoAngular/src/app/app.routes.ts
+++ Proyecto-CursoAngular/src/app/app.routes.ts
@@ -1,16 +1,13 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from
-'./pages/home/home';
-import { MovieListComponent } from
-'./pages/movie-list/movie-list';
-import { FormPageComponent } from
-'./pages/form-page/form-page';
+import { HomeComponent } from './pages/home/home';
+import { MovieListComponent } from './pages/movie-list/movie-list';
+import { FormPageComponent } from './pages/form-page/form-page';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details';
 
 export const routes: Routes = [
-  { path: '', component:
-HomeComponent },
+  { path: '', component: HomeComponent },
   { path: 'movies', component: MovieListComponent },
-  { path:
-'create', component: FormPageComponent },
+  { path: 'create', component: FormPageComponent },
+  { path: 'movies/:movieName', component: MovieDetailsComponent },
 ];
--- Proyecto-CursoAngular/src/app/pages/movie-list/movie-list.ts
+++ Proyecto-CursoAngular/src/app/pages/movie-list/movie-list.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header';
 import { MovieService } from '../../services/movie.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterLink],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.css',
 })
