2c9a0c91cc511a2360d87ebfb323bbab84460e81
Jorge Pérez <perezlopezj07@hotmail.com>
2026-07-05 19:09:44 +0200

    Merge Squash: Add Testing with Karma and Jasmine (#24)

--- Proyecto-CursoAngular/src/app/app.spec.ts
+++ Proyecto-CursoAngular/src/app/app.spec.ts
@@ -2,6 +2,7 @@
 import { AppComponent } from './app';
 
 describe('AppComponent', () => {
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
@@ -9,8 +10,61 @@ describe('AppComponent', () => {
   });
 
   it('should create the app', () => {
+
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+    
   });
+
+  it('title should be correct', () => {
+
+     const fixture = TestBed.createComponent(AppComponent);
+
+     const title = fixture.nativeElement.querySelector('h1');
+
+      expect(title.textContent).toBe('Testing Angular with Jasmine');
+
+  })
+
+  it('form should be invalid if required fields are empty', () => {
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.movieForm.controls['name'].setValue('');
+    app.movieForm.controls['duration'].setValue('');
+
+     expect(app.movieForm.valid).toBeFalsy();
+
+  })
+
+   it('form should be valid if required fields are empty', () => {
+
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.movieForm.controls['name'].setValue('Star Wars');
+    app.movieForm.controls['duration'].setValue('100');
+
+     expect(app.movieForm.valid).toBeTruthy();
+
+  })
+
+  it('btn should send the form when clicked', () => {
+    
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    const sendBtn = fixture.nativeElement.querySelector('#send-btn');
+    sendBtn.click();
+
+    expect(app.sentMsg).toBe('Enviado');
+
+    fixture.detectChanges();
+
+    const sentMsg = fixture.nativeElement.querySelector('#send-msg');
+    expect(sentMsg.textContent).toBe('Enviado');
+
+  })
 });
