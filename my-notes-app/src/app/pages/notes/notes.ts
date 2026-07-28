85710a3df0788b9ab10c85b1af188213afc0027e
Jorge Pérez <perezlopezj07@hotmail.com>
2026-07-28 11:16:03 +0200

    Merge Squash: Add New Page From 0 - Part 2 (#26)
    Add Part 2 for the page of notes.

--- my-notes-app/src/app/pages/notes/notes.ts
+++ my-notes-app/src/app/pages/notes/notes.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import  {Header} from '../../components/header/header';
 import { NoteService} from '../../services/note';
 import { NoteCard } from '../../components/note-card/note-card';
+import { CreateNote } from '../../components/create-note/create-note';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [Header, NoteCard],
+  imports: [Header, NoteCard, CreateNote],
   templateUrl: './notes.html',
   styleUrl: './notes.css',
 })
